2b4fcbf0a27669890678d81bda17f2c3bc5d18ac
edukleber100 <edukleber100@gmail.com>
2023-10-30 15:07:23 -0300

    require validador e navegação entre as páginas

--- valida_login.php
+++ valida_login.php
@@ -1,26 +1,32 @@
 <?php
 
-//variavel verifica  se a autenticacao foi realizada
-$usuario_autenticado = false;
+    session_start();
 
-//usuarios do sistema
-$usuarios_app = array(
-    array('email' => 'teste@teste.com.br', 'senha' => '1509')
-);
 
-foreach($usuarios_app as $user) {
+    //variavel verifica  se a autenticacao foi realizada
+    $usuario_autenticado = false;
 
-   if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha'] ){
-        $usuario_autenticado = true;
-   }
+    //usuarios do sistema
+    $usuarios_app = array(
+        array('email' => 'teste@teste.com.br', 'senha' => '1509')
+    );
 
-}
+    foreach($usuarios_app as $user) {
 
-if($usuario_autenticado){
-    echo 'Usuário autenticado';
-} else {
-    header('Location: index.php?login=erro');
-}
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha'] ){
+            $usuario_autenticado = true;
+    }
+
+    }
+
+    if($usuario_autenticado){
+        echo 'Usuário autenticado';
+        $_SESSION['autenticado'] = 'SIM';
+        header('Location: home.php');
+    } else {
+        $_SESSION['autenticado'] = 'NAO';
+        header('Location: index.php?login=erro');
+    }
 
 
 ?>
